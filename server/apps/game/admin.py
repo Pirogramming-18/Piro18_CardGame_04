8508282aecbde1929d77c608e93684c5323589bf
spark2357 <psbted@naver.com>
2023-01-19 16:45:29 +0900

    [feat] 모델 완료

--- server/apps/game/admin.py
+++ server/apps/game/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Play
 
 admin.site.register(User)
+admin.site.register(Play)
--- server/apps/game/models.py
+++ server/apps/game/models.py
@@ -11,3 +11,23 @@ class User(AbstractUser):
     gender = None
     job = None
     desc = None
+    # 점수
+    score = models.IntegerField(default=0)
+
+class Play(models.Model):
+    how_to_win = ["숫자가 더 작은 사람이 대결에서 이깁니다", "숫자가 더 큰 사람이 대결에서 이깁니다"]
+    # 공격한 사람 -> 처음에 정해짐
+    attack_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="attack")
+    # 공격당한 사람 -> 처음에 정해짐
+    defend_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="defend")
+    # 공격한 사람 카드 -> 처음에 정해짐
+    attack_card = models.IntegerField()
+    # 공격당한 사람 카드 -> 반격하기 할 때 업데이트
+    defend_card = models.IntegerField()
+    # 반격하기 여부
+    accept = models.BooleanField(default=False)
+    # 결과
+    winner = models.IntegerField()
+    # 이기는 기준
+    rule = models.IntegerField()
+    pass
